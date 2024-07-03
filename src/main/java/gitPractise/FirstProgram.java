45ec45cfb928fad96896fc604829b04e413193f0
Mahendra <mudiyammahendra123@gmail.com>
2024-07-03 11:02:41 +0530

    Pallindrome Program

---
+++ src/main/java/gitPractise/FirstProgram.java
@@ -0,0 +1,28 @@
+package gitPractise;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		int num=151;
+		int sum=0;
+		int rem;
+		int temp = num;
+		while(num>0)
+		{
+			rem=num%10;
+			sum=sum*10+rem;
+			num=num/10;
+		}
+		if(temp==sum)
+		{
+			System.out.println("given number is palindrame");
+		}
+		else
+		{
+			System.out.println("given number is not palindrame");
+		}
+
+	}
+
+}
