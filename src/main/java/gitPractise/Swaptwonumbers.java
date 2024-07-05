d205181d2a868c3937b000bca862fb15246bace0
Mahendra <mudiyammahendra123@gmail.com>
2024-07-05 08:49:23 +0530

    swap two numbers

--- src/main/java/gitPractise/Swaptwonumbers.java
+++ src/main/java/gitPractise/Swaptwonumbers.java
@@ -1,9 +1,9 @@
 package gitPractise;
 public class Swaptwonumbers {
 	public static void main(String[] args) {
-		int a=10;//Mahendra
+		int a=10;
 		int b=20;
-		a=a+b;//sanjeeva
+		a=a+b;
 		b=a-b;
 		a=a-b;
 		System.out.println("a="+a +"b="+b);
