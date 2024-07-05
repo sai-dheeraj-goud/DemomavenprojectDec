2700d9af17e96a9aaeff022e58eb0317db396d1f
Mahendra <151931615+mudiyam@users.noreply.github.com>
2024-07-05 08:48:31 +0530

    Update Swaptwonumbers.java

--- src/main/java/gitPractise/Swaptwonumbers.java
+++ src/main/java/gitPractise/Swaptwonumbers.java
@@ -1,8 +1,8 @@
 package gitPractise;
 public class Swaptwonumbers {
 	public static void main(String[] args) {
-		int a=10;//Mahendra
-		int b=20;
+		int a=20;//Mahendra
+		int b=40;
 		a=a+b;//sanjeeva
 		b=a-b;
 		a=a-b;
